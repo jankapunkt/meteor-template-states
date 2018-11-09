af10067c1b9f25670fff054d3ea6502dae478291
Jan Küster <jkuester@uni-bremen.de>
2018-11-09 09:16:50 +0100

    package 0.2.2

--- package.js
+++ package.js
@@ -1,39 +1,38 @@
+/* eslint-env meteor */
 Package.describe({
-	name: 'jkuester:template-states',
-	summary: 'Template states for Blaze',
-	git: 'https://github.com/gwendall/meteor-template-states.git',
-	version: '0.2.1'
-});
+  name: 'jkuester:template-states',
+  summary: 'Template states for Blaze',
+  git: 'https://github.com/gwendall/meteor-template-states.git',
+  version: '0.2.2'
+})
 
 Package.onUse(function (api, where) {
-	api.versionsFrom('1.5');
-	api.use('ecmascript');
-	api.use('random');
-	api.use([
-		'blaze@2.3.2',
-		'reactive-dict@1.2.0',
-		'templating@1.3.2'
-	], 'client');
+  api.versionsFrom('1.5')
+  api.use('ecmascript')
+  api.use('random')
+  api.use([
+    'blaze@2.3.2',
+    'reactive-dict@1.2.0',
+    'templating@1.3.2'
+  ], 'client')
 
-	api.addFiles([
-		'lib.js',
-	], 'client');
-});
+  api.addFiles([
+    'lib.js'
+  ], 'client')
+})
 
 Package.onTest(function (api) {
-	api.use([
-		'random',
-		'ecmascript',
-		'practicalmeteor:chai',
-		'practicalmeteor:mocha',
-		'coffeescript@1.12.7_3',
-	]);
-	api.use([
-		'blaze@2.3.2',
-		'reactive-dict@1.2.0',
-		'templating@1.3.2',
-		'jkuester:template-states',
-	], 'client');
-
-	api.mainModule('lib.tests.js',"client");
-});
+  api.use([
+    'random',
+    'ecmascript',
+    'practicalmeteor:chai',
+    'cultofcoders:mocha'
+  ])
+  api.use([
+    'blaze@2.3.2',
+    'reactive-dict@1.2.0',
+    'templating@1.3.2',
+    'jkuester:template-states'
+  ], 'client')
+  api.mainModule('lib.tests.js', 'client')
+})
